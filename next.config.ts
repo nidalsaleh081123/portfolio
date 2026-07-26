d2c2ee459386d8f00a5730185f21b8f1c1ba1b44
Nidal Saleh <nidalsaleh081123@gmail.com>
2026-07-26 12:26:06 +0300

    Configure GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,41 @@
+// // // import type { NextConfig } from "next";
+
+// // // const nextConfig: NextConfig = {
+// // //   /* config options here */
+// // // };
+
+// // // export default nextConfig;
+
 // // import type { NextConfig } from "next";
 
-// // const nextConfig: NextConfig = {
-// //   /* config options here */
-// // };
+// // const nextConfig: NextConfig = {};
 
 // // export default nextConfig;
-
 // import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {};
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+// };
 
 // export default nextConfig;
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
+
+  ...(isProd && {
+    basePath: "/portfolio",
+    assetPrefix: "/portfolio/",
+  }),
 };
 
 export default nextConfig;
