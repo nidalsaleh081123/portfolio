fbdbeb3571d68821fa7907c170005fdd40ca4c1e
nidalsaleh081123 <nidalsaleh081123@gmail.com>
2026-07-26 12:52:50 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,11 @@
-// // // import type { NextConfig } from "next";
-
-// // // const nextConfig: NextConfig = {
-// // //   /* config options here */
-// // // };
-
-// // // export default nextConfig;
-
-// // import type { NextConfig } from "next";
-
-// // const nextConfig: NextConfig = {};
-
-// // export default nextConfig;
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   output: "export",
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// export default nextConfig;
 import type { NextConfig } from "next";
-
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-
+  basePath: "/portfolio",
   images: {
     unoptimized: true,
   },
-
-  ...(isProd && {
-    basePath: "/portfolio",
-    assetPrefix: "/portfolio/",
-  }),
 };
 
 export default nextConfig;
